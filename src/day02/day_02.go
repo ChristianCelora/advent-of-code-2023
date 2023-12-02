c7d8c77ad124862f60737d352525207e7c41207b
christian <christian.celora@leadsbridge.com>
2023-12-02 14:28:37 +0100

    day02

--- src/day02/day_02.go
+++ src/day02/day_02.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"adventcode/reader"
+	"fmt"
 	"strconv"
 	"strings"
 )
@@ -13,13 +15,13 @@ const (
 
 func ReadGameID(line string) int {
 	first_space := strings.Index(line, " ")
-	first_semicolon := strings.Index(line, ":")
+	first_colon := strings.Index(line, ":")
 
-	if first_space == -1 || first_semicolon == -1 {
+	if first_space == -1 || first_colon == -1 {
 		panic("error reading game ID")
 	}
 
-	id, err := strconv.Atoi(line[first_space+1 : first_semicolon])
+	id, err := strconv.Atoi(line[first_space+1 : first_colon])
 	if err != nil {
 		panic(err)
 	}
@@ -31,12 +33,91 @@ type Set struct {
 	blue  int
 	green int
 	red   int
+	other int
 }
 
-func ReadCubeSets() []Set {
-	panic("to do")
+func ReadCubeSets(line string) []Set {
+	var res []Set
+	first_colon := strings.Index(line, ":")
+	rounds := strings.Split(line[first_colon+1:], ";")
+
+	for _, r := range rounds {
+		set := Set{}
+		r := strings.TrimSpace(r)
+		cubes := strings.Split(r, ",")
+		for _, cube := range cubes {
+			cube = strings.TrimSpace(cube)
+			c := strings.Split(cube, " ")
+			n, err := strconv.Atoi(c[0])
+			if err != nil {
+				panic(err)
+			}
+			color := c[1]
+			switch color {
+			case "blue":
+				set.blue = n
+			case "green":
+				set.green = n
+			case "red":
+				set.red = n
+			default:
+				set.other = n
+			}
+		}
+		res = append(res, set)
+	}
+
+	return res
+}
+
+func IsGameValid(game_set []Set) bool {
+	for _, set := range game_set {
+		if set.blue > MAX_BLUE ||
+			set.green > MAX_GREEN ||
+			set.red > MAX_RED ||
+			set.other > 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func GetCubesForValidGame(game_set []Set) Set {
+	var max_cubes Set
+	for _, set := range game_set {
+		if set.blue > max_cubes.blue {
+			max_cubes.blue = set.blue
+		}
+		if set.green > max_cubes.green {
+			max_cubes.green = set.green
+		}
+		if set.red > max_cubes.red {
+			max_cubes.red = set.red
+		}
+	}
+
+	return max_cubes
 }
 
 func main() {
-	// lines := reader.ReadLines("./day02/data/input1_1.txt")
+	var sum int
+	var sum_power int
+	// Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+	lines := reader.ReadLines("./day02/data/input1_2.txt")
+	for _, line := range lines {
+		game_id := ReadGameID(line)
+		cube_sets := ReadCubeSets(line)
+		// star 1
+		if IsGameValid(cube_sets) {
+			sum += game_id
+		}
+		// star 2
+		cubes_valid_game := GetCubesForValidGame(cube_sets)
+		power_set := cubes_valid_game.blue * cubes_valid_game.green * cubes_valid_game.red
+		sum_power += power_set
+	}
+
+	fmt.Printf("Game valid ID sum is %d\n", sum)
+	fmt.Printf("Game power set sum is %d\n", sum_power)
 }
--- src/day02/day_02_test.go
+++ src/day02/day_02_test.go
@@ -28,3 +28,236 @@ func TestReadGameID(t *testing.T) {
 		}
 	}
 }
+
+func TestReadCubeSets(t *testing.T) {
+	tests := []struct {
+		input_line     string
+		expected_value []Set
+	}{
+		{
+			input_line: "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+			expected_value: []Set{
+				{
+					blue: 3,
+					red:  4,
+				},
+				{
+					blue:  6,
+					green: 2,
+					red:   1,
+				},
+				{
+					green: 2,
+				},
+			},
+		},
+		{
+			input_line: "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+			expected_value: []Set{
+				{
+					blue:  1,
+					red:   0,
+					green: 2,
+				},
+				{
+					blue:  4,
+					red:   1,
+					green: 3,
+				},
+				{
+					blue:  1,
+					red:   0,
+					green: 1,
+				},
+			},
+		},
+		{
+			input_line: "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+			expected_value: []Set{
+				{
+					blue:  6,
+					red:   20,
+					green: 8,
+				},
+				{
+					blue:  5,
+					red:   4,
+					green: 13,
+				},
+				{
+					blue:  0,
+					red:   1,
+					green: 5,
+				},
+			},
+		},
+		{
+			input_line: "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+			expected_value: []Set{
+				{
+					blue:  6,
+					red:   3,
+					green: 1,
+				},
+				{
+					blue:  0,
+					red:   6,
+					green: 3,
+				},
+				{
+					blue:  15,
+					red:   14,
+					green: 3,
+				},
+			},
+		},
+		{
+			input_line: "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+			expected_value: []Set{
+				{
+					blue:  1,
+					red:   6,
+					green: 3,
+				},
+				{
+					blue:  2,
+					red:   1,
+					green: 2,
+				},
+			},
+		},
+		{
+			input_line: "Game 5: 6 red, 1 blue, 3 green, 1 yellow; 2 blue, 1 red, 2 green",
+			expected_value: []Set{
+				{
+					blue:  1,
+					red:   6,
+					green: 3,
+					other: 1,
+				},
+				{
+					blue:  2,
+					red:   1,
+					green: 2,
+				},
+			},
+		},
+	}
+
+	for _, test := range tests {
+		sets := ReadCubeSets(test.input_line)
+		for i, set := range sets {
+			if set.blue != test.expected_value[i].blue ||
+				set.green != test.expected_value[i].green ||
+				set.red != test.expected_value[i].red {
+				t.Fatalf("expected %v value. actual %v for line %s", test.expected_value, sets, test.input_line)
+			}
+		}
+	}
+}
+
+func TestIsGameValid(t *testing.T) {
+	tests := []struct {
+		input_set      []Set
+		expected_value bool
+	}{
+		{
+			input_set: []Set{
+				{
+					blue: 3,
+					red:  4,
+				},
+				{
+					blue:  6,
+					green: 2,
+					red:   1,
+				},
+				{
+					green: 2,
+				},
+			},
+			expected_value: true,
+		},
+		{
+			input_set: []Set{
+				{
+					blue: 20,
+					red:  4,
+				},
+			},
+			expected_value: false,
+		},
+		{
+			input_set: []Set{
+				{
+					blue:  3,
+					red:   4,
+					other: 1,
+				},
+			},
+			expected_value: false,
+		},
+	}
+
+	for _, test := range tests {
+		is_valid := IsGameValid(test.input_set)
+		if is_valid != test.expected_value {
+			t.Fatalf("expected %t value. actual %t for set %v", test.expected_value, is_valid, test.input_set)
+		}
+	}
+}
+
+func TestGetCubesForValidGame(t *testing.T) {
+	tests := []struct {
+		input_set      []Set
+		expected_value Set
+	}{
+		{
+			input_set: []Set{
+				{
+					blue: 3,
+					red:  4,
+				},
+				{
+					blue:  6,
+					green: 2,
+					red:   1,
+				},
+				{
+					green: 2,
+				},
+			},
+			expected_value: Set{
+				blue:  6,
+				red:   4,
+				green: 2,
+			},
+		},
+		{
+			input_set: []Set{
+				{
+					blue: 20,
+					red:  4,
+				},
+				{
+					blue: 12,
+					red:  15,
+				},
+			},
+			expected_value: Set{
+				blue:  20,
+				red:   15,
+				green: 0,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		cubes := GetCubesForValidGame(test.input_set)
+		if cubes.blue != test.expected_value.blue ||
+			cubes.green != test.expected_value.green ||
+			cubes.red != test.expected_value.red {
+			t.Fatalf("expected %v value. actual %v for set %v", test.expected_value, cubes, test.input_set)
+		}
+	}
+}
