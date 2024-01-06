9419b1f40590526503bdc2bb8e8e56506ffe6ef4
christian <christian.celora@leadsbridge.com>
2024-01-06 22:41:14 +0100

    faster but still fail

--- src/day14/day_14.go
+++ src/day14/day_14.go
@@ -10,8 +10,8 @@ const (
 	// N = 140
 
 	// N_CICLES = 3 // test
-	N_CICLES = 1_000_000 // test
-	// N_CICLES = 1_000_000_000
+	// N_CICLES = 1_000_000 // test
+	N_CICLES = 1_000_000_000
 )
 
 func getPlatformMatrix(lines []string) [N][N]string {
@@ -29,7 +29,7 @@ func getPlatformMatrix(lines []string) [N][N]string {
 	return platform
 }
 
-func tiltPlatformNorth(platform [N][N]string) {
+func tiltPlatformNorth(platform *[N][N]string) {
 	// save top min positions
 	min_free_space := make([]int, len(platform[0]))
 
@@ -44,6 +44,69 @@ func tiltPlatformNorth(platform [N][N]string) {
 					platform[i][j] = "."
 				}
 				min_free_space[j]++
+			}
+		}
+	}
+}
+
+func tiltPlatformEast(platform *[N][N]string) {
+	min_free_space := make([]int, len(platform))
+	for i := 0; i < len(min_free_space); i++ {
+		min_free_space[i] = len(platform) - 1
+	}
+
+	for i := 0; i < len(platform); i++ {
+		for j := len(platform[0]) - 1; j >= 0; j-- {
+			if platform[i][j] == "#" {
+				min_free_space[i] = j - 1
+			} else if platform[i][j] == "O" {
+				if j < min_free_space[i] {
+					// tilt right
+					platform[i][min_free_space[i]] = "O"
+					platform[i][j] = "."
+				}
+				min_free_space[i]--
+			}
+		}
+	}
+}
+
+func tiltPlatformSouth(platform *[N][N]string) {
+	min_free_space := make([]int, len(platform))
+	for i := 0; i < len(min_free_space); i++ {
+		min_free_space[i] = len(platform) - 1
+	}
+
+	for i := len(platform[0]) - 1; i >= 0; i-- {
+		for j := len(platform[0]) - 1; j >= 0; j-- {
+			if platform[i][j] == "#" {
+				min_free_space[j] = i - 1
+			} else if platform[i][j] == "O" {
+				if i < min_free_space[j] {
+					// tilt bottom
+					platform[min_free_space[j]][j] = "O"
+					platform[i][j] = "."
+				}
+				min_free_space[j]--
+			}
+		}
+	}
+}
+
+func tiltPlatformWest(platform *[N][N]string) {
+	min_free_space := make([]int, len(platform))
+
+	for i := 0; i < len(platform); i++ {
+		for j := 0; j < len(platform); j++ {
+			if platform[i][j] == "#" {
+				min_free_space[i] = j + 1
+			} else if platform[i][j] == "O" {
+				if j > min_free_space[i] {
+					// tilt left
+					platform[i][min_free_space[i]] = "O"
+					platform[i][j] = "."
+				}
+				min_free_space[i]++
 			}
 		}
 	}
@@ -118,34 +181,17 @@ func main() {
 	// fmt.Printf("sum of platform load is %d", sumPlatformLoad(platform))
 
 	// step 2
-	cycle := 0
 	for i := 0; i < N_CICLES; i++ {
 		fmt.Printf("cicle %d\n", i)
-		// bk_platform := copyMatrix[string](platform)
-		for j := 0; j < 4; j++ {
-			tiltPlatformNorth(platform)
-			platform = rotateMatrix(platform)
-		}
-		// fmt.Printf("platform after cicle %d: \n", i+1)
-		// printPlatform(platform)
-
-		// has_changes := false
-		// for i := 0; i < len(platform); i++ {
-		// 	for j := 0; j < len(platform[i]); j++ {
-		// 		if platform[i][j] != bk_platform[i][j] {
-		// 			has_changes = true
-		// 			break
-		// 		}
-		// 	}
-		// 	if has_changes {
-		// 		break
-		// 	}
+		// for j := 0; j < 4; j++ {
+		// 	tiltPlatformNorth(&platform)
+		// 	// platform = rotateMatrix(platform)
 		// }
-		// cycle = i + 1
-		// if !has_changes {
-		// 	break
-		// }
+		tiltPlatformNorth(&platform)
+		tiltPlatformWest(&platform)
+		tiltPlatformSouth(&platform)
+		tiltPlatformEast(&platform)
 	}
-	printPlatform(platform)
-	fmt.Printf("sum of platform load after %d cycles is %d\n", cycle, sumPlatformLoad(platform))
+	// printPlatform(platform)
+	fmt.Printf("sum of platform load after %d cycles is %d\n", N_CICLES, sumPlatformLoad(platform))
 }
